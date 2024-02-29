372af4ff7955922b2575241d60b2982543b91398
Oğuzhan <oguzhan.cevik@st.uskudar.edu.tr>
2024-02-29 09:58:30 +0300

    length test and valid test added.

--- tests/Unit.Test/ResourceCostType/CreateResourceCostTypeCommandHandlerTest.cs
+++ tests/Unit.Test/ResourceCostType/CreateResourceCostTypeCommandHandlerTest.cs
@@ -4,6 +4,7 @@ using Application.Feature.AbilityFeatures.ResourceCostType.Rules;
 using Application.Service.AbilityServices.ResourceCostTypeService;
 using AutoMapper;
 using Moq;
+using System.Xml.Linq;
 using Xunit;
 
 namespace Unit.Test.ResourceCostType;
@@ -27,7 +28,7 @@ public class CreateResourceCostTypeCommandHandlerTest
     [InlineData("Wood")]
     [InlineData("Stone")]
     [InlineData("Iron")]
-    public async Task ResourceCostTypeHandle_ValidRequest_ReturnsResponse(string name)
+    public async Task ResourceCostTypeHandler_ValidRequest_ReturnsResponse(string name)
     {
 
         var handler = new CreateResourceCostTypeCommandHandler(
@@ -77,6 +78,179 @@ public class CreateResourceCostTypeCommandHandlerTest
         Assert.Equal(expectedResponse, response);
 
         _resourceCostTypeServiceMock.Verify(m => m.Create(expectedResourceCostType), Times.Once);
+    }
+
+
+
+    [Theory]
+    [InlineData("Hourly")]
+    [InlineData("Wood")]
+    [InlineData("Stone")]
+    [InlineData("Iron")]
+    public async Task ValidateNameLengthInRange_ValidName_ReturnsTrue(string name)
+    {
+        var handler = new CreateResourceCostTypeCommandHandler(
+           _mapperMock.Object,
+           _resourceCostTypeServiceMock.Object,
+           _resourceCostTypeBusinessRulesMock.Object
+       );
+
+        var request = new CreateResourceCostTypeCommandRequest
+        {
+            CreateResourcesCostTypeDto = new CreateResourcesCostTypeDto
+            {
+                Name = name
+            }
+        };
+
+        var expectedResourceCostType = new Domain.Abilities.ResourceCostType
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = request.CreateResourcesCostTypeDto.Name,
+            Status = true,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now
+        };
+
+        var expectedResponse = new CreateResourceCostTypeCommandResponse
+        {
+
+            Id = expectedResourceCostType.Id,
+            Name = expectedResourceCostType.Name,
+            Status = true,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now,
+        };
+
+        _mapperMock.Setup(m => m.Map<Domain.Abilities.ResourceCostType>(request.CreateResourcesCostTypeDto))
+            .Returns(expectedResourceCostType);
+
+        _resourceCostTypeServiceMock.Setup(m => m.Create(expectedResourceCostType))
+            .ReturnsAsync(expectedResourceCostType);
+
+        _mapperMock.Setup(m => m.Map<CreateResourceCostTypeCommandResponse>(expectedResourceCostType))
+            .Returns(expectedResponse);
+
+        Assert.True(ValidateNameLengthInRange(name));
+
+    }
+
+
+    [Theory]
+    [InlineData("Ho")]
+    [InlineData("Wo")]
+    [InlineData("S")]
+    [InlineData("I")]
+    public async Task ValidateNameLengthInRange_InvalidNameShort_ReturnsFalse(string name)
+    {
+        var handler = new CreateResourceCostTypeCommandHandler(
+          _mapperMock.Object,
+          _resourceCostTypeServiceMock.Object,
+          _resourceCostTypeBusinessRulesMock.Object
+      );
+
+        var request = new CreateResourceCostTypeCommandRequest
+        {
+            CreateResourcesCostTypeDto = new CreateResourcesCostTypeDto
+            {
+                Name = name
+            }
+        };
+
+        var expectedResourceCostType = new Domain.Abilities.ResourceCostType
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = request.CreateResourcesCostTypeDto.Name,
+            Status = true,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now
+        };
+
+        var expectedResponse = new CreateResourceCostTypeCommandResponse
+        {
+
+            Id = expectedResourceCostType.Id,
+            Name = expectedResourceCostType.Name,
+            Status = true,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now,
+        };
+
+        _mapperMock.Setup(m => m.Map<Domain.Abilities.ResourceCostType>(request.CreateResourcesCostTypeDto))
+            .Returns(expectedResourceCostType);
+
+        _resourceCostTypeServiceMock.Setup(m => m.Create(expectedResourceCostType))
+            .ReturnsAsync(expectedResourceCostType);
+
+        _mapperMock.Setup(m => m.Map<CreateResourceCostTypeCommandResponse>(expectedResourceCostType))
+            .Returns(expectedResponse);
+
+        Assert.False(ValidateNameLengthInRange(name));
+
+
+    }
+
+
+
+    [Theory]
+    [InlineData("ThisIsAVeryLongNameThatExceedsTheMaxLengthLimit")]
+
+    public void ValidateNameLengthInRange_InvalidNameLong_ReturnsFalse(string name)
+    {
+
+        var handler = new CreateResourceCostTypeCommandHandler(
+         _mapperMock.Object,
+         _resourceCostTypeServiceMock.Object,
+         _resourceCostTypeBusinessRulesMock.Object
+     );
+
+        var request = new CreateResourceCostTypeCommandRequest
+        {
+            CreateResourcesCostTypeDto = new CreateResourcesCostTypeDto
+            {
+                Name = name
+            }
+        };
+
+        var expectedResourceCostType = new Domain.Abilities.ResourceCostType
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = request.CreateResourcesCostTypeDto.Name,
+            Status = true,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now
+        };
+
+        var expectedResponse = new CreateResourceCostTypeCommandResponse
+        {
+
+            Id = expectedResourceCostType.Id,
+            Name = expectedResourceCostType.Name,
+            Status = true,
+            IsDeleted = false,
+            CreatedDate = DateTime.Now,
+        };
+
+        _mapperMock.Setup(m => m.Map<Domain.Abilities.ResourceCostType>(request.CreateResourcesCostTypeDto))
+            .Returns(expectedResourceCostType);
+
+        _resourceCostTypeServiceMock.Setup(m => m.Create(expectedResourceCostType))
+            .ReturnsAsync(expectedResourceCostType);
+
+        _mapperMock.Setup(m => m.Map<CreateResourceCostTypeCommandResponse>(expectedResourceCostType))
+            .Returns(expectedResponse);
+
+        Assert.False(ValidateNameLengthInRange(name));
+    }
+
+
+    private bool ValidateNameLengthInRange(string name)
+    {
+        int minLength = 3;
+        int maxLength = 20;
+
+        int nameLength = name.Length;
+        return nameLength >= minLength && nameLength <= maxLength;
     }
 }
 
